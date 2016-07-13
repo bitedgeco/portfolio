1049066e376fdae2019719f54cba64a4f1d4b6b0
James <canning.jimmy@gmail.com>
2016-07-12 19:28:47 -0700

    Added prodjects

---
+++ public/js/main.js
@@ -0,0 +1,18 @@
+"use strict";
+(function() {
+
+	console.log("Hello from JS");
+
+	$("#prodject-head-1").html("<a href='https://www.bitedge.co/' target='_blank'>Bitedge</a>");
+	$("#project-box-1").html("<a href='http://www.bitedge.co/' target='_blank'><img class='bootstrap-img project-img' src='img/bitedge.png'></a>");
+	$("#project-p-1").html("Helping you win bitcoin!");
+
+	$("#prodject-head-2").html("<a href='http://www.bitaverage.com/' target='_blank'>Bitaverage</a>");
+	$("#project-box-2").html("<a href='http://www.bitaverage.com/' target='_blank'><img class='bootstrap-img project-img' src='img/bitaverage2.png'></a>");
+	$("#project-p-2").html("Average price of bitcoin for any timeframe");
+
+	$("#prodject-head-3").html("<a href='#' target='_blank'>Shakespearean insult generator</a>");
+	$("#project-box-3").html("<a href='#' target='_blank'><img class='bootstrap-img project-img' src='img/shakespearean.jpg'></a>");
+	$("#project-p-3").html("Thou art a ...");
+
+})();
