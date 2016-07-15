f8049931194eebdd9c9ec54a33759d3e7f3ccf33
James <canning.jimmy@gmail.com>
2016-07-14 20:13:47 -0700

    Output projects with handlebars

--- public/js/main.js
+++ public/js/main.js
@@ -1,18 +1,31 @@
 "use strict";
 (function() {
 
-	console.log("Hello from JS");
+	var projects = [];
 
-	$("#prodject-head-1").html("<a href='https://www.bitedge.co/' target='_blank'>Bitedge</a>");
-	$("#project-box-1").html("<a href='http://www.bitedge.co/' target='_blank'><img class='bootstrap-img project-img' src='img/bitedge.png'></a>");
-	$("#project-p-1").html("Helping you win bitcoin!");
+	// function Project (opts) {
+	// 	for (key in opts) this[key] = opts[key];
+	// };
 
-	$("#prodject-head-2").html("<a href='http://www.bitaverage.com/' target='_blank'>Bitaverage</a>");
-	$("#project-box-2").html("<a href='http://www.bitaverage.com/' target='_blank'><img class='bootstrap-img project-img' src='img/bitaverage2.png'></a>");
-	$("#project-p-2").html("Average price of bitcoin for any timeframe");
+	function Project (opts) {
+		this.name = opts.name;
+		this.img = opts.img;
+		this.tag = opts.tag;
+		this.link = opts.link;
+	}
 
-	$("#prodject-head-3").html("<a href='#' target='_blank'>Shakespearean insult generator</a>");
-	$("#project-box-3").html("<a href='#' target='_blank'><img class='bootstrap-img project-img' src='img/shakespearean.jpg'></a>");
-	$("#project-p-3").html("Thou art a ...");
+	Project.prototype.toHtml = function() {
+		var source = $('#project-template').html();
+		var template = Handlebars.compile(source);
+	return template(this);
+	};
+
+	projectData.forEach(function(projectObject) {
+		projects.push(new Project(projectObject));
+	});
+
+	projects.forEach(function(NewProjectObject){
+		$('#projects').append(NewProjectObject.toHtml());
+	});
 
 })();
