b8e3bf34e779bd0b2c292b9f5750203b01e40d11
James <canning.jimmy@gmail.com>
2016-07-15 09:36:45 -0700

    Fixed tab vs 4 spaces indenting

--- public/js/main.js
+++ public/js/main.js
@@ -1,31 +1,31 @@
 "use strict";
 (function() {
 
-	var projects = [];
+    var projects = [];
 
-	// function Project (opts) {
-	// 	for (key in opts) this[key] = opts[key];
-	// };
+    // function Project (opts) {
+    //     for (key in opts) this[key] = opts[key];
+    // };
 
-	function Project (opts) {
-		this.name = opts.name;
-		this.img = opts.img;
-		this.tag = opts.tag;
-		this.link = opts.link;
-	}
+    function Project (opts) {
+        this.name = opts.name;
+        this.img = opts.img;
+        this.tag = opts.tag;
+        this.link = opts.link;
+    }
 
-	Project.prototype.toHtml = function() {
-		var source = $('#project-template').html();
-		var template = Handlebars.compile(source);
-	return template(this);
-	};
+    Project.prototype.toHtml = function() {
+        var source = $('#project-template').html();
+        var template = Handlebars.compile(source);
+    return template(this);
+    };
 
-	projectData.forEach(function(projectObject) {
-		projects.push(new Project(projectObject));
-	});
+    projectData.forEach(function(projectObject) {
+        projects.push(new Project(projectObject));
+    });
 
-	projects.forEach(function(NewProjectObject){
-		$('#projects').append(NewProjectObject.toHtml());
-	});
+    projects.forEach(function(NewProjectObject){
+        $('#projects').append(NewProjectObject.toHtml());
+    });
 
 })();
