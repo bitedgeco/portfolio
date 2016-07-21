e6b3d493f84744621850d34ff75ae13a105e5e3d
James <canning.jimmy@gmail.com>
2016-07-20 23:48:29 -0700

    Output progess bars with handelbars and improved styling.

---
+++ public/js/progressBar.js
@@ -0,0 +1,50 @@
+progressBarData = [
+    {
+        name:    'Search engine optimization',
+        score:   70
+    },
+    {
+        name:    'Project managment',
+        score:   60
+    },
+    {
+        name:    'WordPress',
+        score:   60
+    },
+    {
+        name:    'Git version control',
+        score:   60
+    },
+    {
+        name:    'Front end JavaScript and JQuery',
+        score:   60
+    },
+    {
+        name:    'CSS and Bootstrap',
+        score:   60
+    },
+    {
+        name:    'PHP and Laravel',
+        score:   50
+    },
+    {
+        name:    'HTML and templating',
+        score:   50
+    },
+    {
+        name:    'MySQL database',
+        score:   40
+    },
+    {
+        name:    'Linux system admin',
+        score:   20
+    },
+    {
+        name:    'Email marketing',
+        score:   20
+    },
+    {
+        name:    'Python and web scraping',
+        score:   10
+    }
+];
