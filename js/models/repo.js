81f35ca86d2874a3ea3eaeb4ebb7fd8f03c71a83
James <canning.jimmy@gmail.com>
2016-07-26 21:01:01 -0700

    clean up indenting and spacing

--- js/models/repo.js
+++ js/models/repo.js
@@ -1,30 +1,32 @@
+'use strict';
 (function(module) {
-  var reposObj = {};
 
-  reposObj.allRepos = [];
+    var reposObj = {};
 
-  reposObj.requestRepos = function(nextFunction) {
+    reposObj.allRepos = [];
 
-    $.ajax({
-      url: 'https://api.github.com/users/bitedgeco/repos' +
-         '?per_page=3' +
-         '&sort=update',
-      type: 'GET',
-      headers: {
-        'Authorization': 'token ' + githubToken,
-      },
-      success: function(data, message, xhr) {
-        reposObj.allRepos = data;
-        nextFunction();
-      }
-    });
-  };
+    reposObj.requestRepos = function(nextFunction) {
+        $.ajax({
+        url: 'https://api.github.com/users/bitedgeco/repos' +
+        '?per_page=3' +
+        '&sort=update',
+        type: 'GET',
+        headers: {
+            'Authorization': 'token ' + githubToken,
+        },
+        success: function(data, message, xhr) {
+            reposObj.allRepos = data;
+            nextFunction();
+            }
+        });
+    };
 
-  reposObj.withTheAttribute = function(myAttr) {
-    return reposObj.allRepos.filter(function(aRepo) {
-      return aRepo[myAttr];
-    });
-  };
+    reposObj.withTheAttribute = function(myAttr) {
+        return reposObj.allRepos.filter(function(aRepo) {
+        return aRepo[myAttr];
+        });
+    };
 
-  module.reposObj = reposObj;
+    module.reposObj = reposObj;
+    
 })(window);
--- js/views/repoView.js
+++ js/views/repoView.js
@@ -1,18 +1,22 @@
+'use strict';
 (function(module) {
-  var repoView = {};
-  var repoCompiler = function(data) {
-    var source = $('#repo-template').html();
-    var template = Handlebars.compile(source);
-    return template(data);
-  };  
 
-  repoView.renderRepos = function() {
-    $('#repo').empty().append(
-      reposObj.withTheAttribute('name')  
-      .map(repoCompiler)
-    );
-  };
+    var repoView = {};
 
-  reposObj.requestRepos(repoView.renderRepos);
-  module.repoView = repoView;
+    var repoCompiler = function(data) {
+        var source = $('#repo-template').html();
+        var template = Handlebars.compile(source);
+        return template(data);
+    };  
+
+    repoView.renderRepos = function() {
+        $('#repo').empty().append(
+        reposObj.withTheAttribute('name')  
+        .map(repoCompiler)
+        );
+    };
+
+    reposObj.requestRepos(repoView.renderRepos);
+    module.repoView = repoView;
+
 })(window);
